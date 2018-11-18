261e1e93596732cce4ca8c67939c5f899a45d3ee
Randy Herasme <flatironschool@Flatirons-MacBook-Air.local>
2018-11-18 18:24:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function printBadges(arr){
+  for (y=0; y<arr.length; y++){
+    console.log(`Welcome ${arr[y]}! You are employee #${y+1}.`)
+  }
+  return arr
+}
+
+function tailsNeverFails(){
+  let y = 0
+  while (Math.random() >= 0.5) {
+    y+=1
+  }
+  return `You got ${y} tails in a row!`
+}
